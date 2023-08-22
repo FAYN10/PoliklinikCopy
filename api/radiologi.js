49b4bb85ded4bab5b70becfb5a02c2bd9d9c83d0
ahmadmm01 <ahmad.musthafa.ft20@mail.umy.ac.id>
2023-08-22 10:52:28 +0700

    add radiologi get api

--- api/radiologi.js
+++ api/radiologi.js
@@ -16,3 +16,43 @@ export function getListBmhpRadiologi(params) {
     params,
   });
 }
+
+export function getListPermintaanPemeriksaanRadiologi(params) {
+  return request({
+    url: `${process.env.NEXT_PUBLIC_GATEWAY_BASE_URL}/rs-service/permintaanpemeriksaanradiologi`,
+    method: "GET",
+    params,
+  });
+}
+
+export function getListHasilPemeriksaanRadiologi(params) {
+  return request({
+    url: `${process.env.NEXT_PUBLIC_GATEWAY_BASE_URL}/rs-service/hasilpemeriksaanradiologi`,
+    method: "GET",
+    params,
+  });
+}
+
+export function getListAsesmenPasienRadiologi(params) {
+  return request({
+    url: `${process.env.NEXT_PUBLIC_GATEWAY_BASE_URL}/rs-service/bmhpradiologi`,
+    method: "GET",
+    params,
+  });
+}
+
+export function getListAsesmenPemeriksaanRadiologi(params) {
+  return request({
+    url: `${process.env.NEXT_PUBLIC_GATEWAY_BASE_URL}/rs-service/asesmenpasienradiologi`,
+    method: "GET",
+    params,
+  });
+}
+
+export function getListAsesmenPemeriksaanRadiologi(params) {
+  return request({
+    url: `${process.env.NEXT_PUBLIC_GATEWAY_BASE_URL}/rs-service/asesmenpemeriksaanradiologi`,
+    method: "GET",
+    params,
+  });
+}
