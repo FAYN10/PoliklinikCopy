6fb023092457512e844fdccf83488df0735843c0
eunannana <95303337+eunannana@users.noreply.github.com>
2023-08-25 17:15:17 +0700

    add permintaanRadiologiTableLayout

---
+++ components/modules/radiologi/permintaanRadiologiTableLayout.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TablePagination,
+} from '@mui/material';
+
+const PermintaanRadiologiTableLayout = ({
+  tableHead,
+  data,
+  page,
+  rowsPerPage,
+  totalRows,
+  onPageChange,
+}) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {tableHead.map((column) => (
+              <TableCell key={column.id}>{column.label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              {tableHead.map((column) => (
+                <TableCell key={column.id}>{item[column.id]}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={totalRows}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={onPageChange}
+      />
+    </TableContainer>
+  );
+};
+
+export default PermintaanRadiologiTableLayout;
